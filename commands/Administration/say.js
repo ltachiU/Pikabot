3bb3bf46285d74b69f1bc47244e516d49749d81c
vaaako <matharth520@gmail.com>
2022-03-27 09:42:58 -0300

    say

---
+++ commands/Administration/say.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "say",
+    category: "Administration",
+    aliases: [],
+    usage: "say <text>",
+    description: ":parrot:",
+    whitelistOnly: true,
+    run: async (client, message, args) => {
+    
+      const text = args.join(" ");
+      if(!args[0]) return message.channel.send("Sem texto pra eu falar");
+
+      message.delete();
+      message.channel.send(text);
+
+  }
+};
